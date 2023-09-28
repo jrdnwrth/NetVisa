30ff76ef639ca78d17c500cdedff79165652588f
MAS <mas@332lab.ru>
2023-09-28 17:10:55 +0700

    Added a serial port class that inherits from VIsa-Session. Added properties for configuring serial port parameters.

---
+++ NetVisa/2_visa_session/SerialVisaSession.cs
@@ -0,0 +1,78 @@
+﻿namespace NetVisa;
+
+public class SerialVisaSession : Visa_Session
+{
+    public string TerminationCharacter { get; set; }
+    public uint BaudRate
+    {
+        get
+        {
+            resource_manager.driver.GetAttributeInt(this.Session.Handle, (uint)NativeVisaAttributes.SerialBaud, out var value);
+            return (uint)value;
+        }
+        set
+        {
+            var baudRate = new UIntPtr(value);
+            resource_manager.driver.SetAttribute(this.Session.Handle, (uint)NativeVisaAttributes.SerialBaud, baudRate);
+        }
+    }
+    public uint DataBits
+    {
+        get
+        {
+            resource_manager.driver.GetAttributeInt(this.Session.Handle, (uint)NativeVisaAttributes.SerialDataBits, out var value);
+            return (uint)value;
+        }
+        set
+        {
+            var dataBits = new UIntPtr(value);
+            resource_manager.driver.SetAttribute(this.Session.Handle, (uint)NativeVisaAttributes.SerialDataBits, dataBits);
+        }
+    }
+
+    public SerialParity Parity
+    {
+        get
+        {
+            resource_manager.driver.GetAttributeInt(this.Session.Handle, (uint)NativeVisaAttributes.SerialParity, out var value);
+            return (SerialParity)value;
+        }
+        set
+        {
+            var parity = new UIntPtr((uint)value);
+            resource_manager.driver.SetAttribute(this.Session.Handle, (uint)NativeVisaAttributes.SerialParity, parity);
+        }
+    }
+
+    public SerialStopBitsMode StopBits
+    {
+        get
+        {
+            resource_manager.driver.GetAttributeInt(this.Session.Handle, (uint)NativeVisaAttributes.SerialStopBits, out var value);
+            return (SerialStopBitsMode)value;
+        }
+        set
+        {
+            var stopBits = new UIntPtr((uint)value);
+            resource_manager.driver.SetAttribute(this.Session.Handle, (uint)NativeVisaAttributes.SerialStopBits, stopBits);
+        }
+    }
+
+    public SerialFlowControlModes FlowControl
+    {
+        get
+        {
+            resource_manager.driver.GetAttributeInt(this.Session.Handle, (uint)NativeVisaAttributes.SerialFlowControl, out var value);
+            return (SerialFlowControlModes)value;
+        }
+        set
+        {
+            var flowControl = new UIntPtr((uint)value);
+            resource_manager.driver.SetAttribute(this.Session.Handle, (uint)NativeVisaAttributes.SerialFlowControl, flowControl);
+        }
+    }
+
+    public SerialVisaSession(string resourceName, Resource_Manager resource_manager) : base(resourceName, resource_manager)
+    {
+    }
+}
